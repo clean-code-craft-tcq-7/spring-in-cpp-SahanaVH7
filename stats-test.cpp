403fa473c0460b4297b399587764a801deb32ede
SahanaVH7 <vhsahana3@gmail.com>
2025-09-06 13:20:56 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,21 +1,23 @@
-#include "stats.h"
-
 #include "gtest/gtest.h"
+#include "stats.h"
 #include <cmath>
+#include <vector>
 
-TEST(Statistics, ReportsAverageMinMax) {
-    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    float epsilon = 0.001;
-    EXPECT_LT(std::abs(computedStats.average - 4.525), epsilon);
-    EXPECT_LT(std::abs(computedStats.max - 8.9), epsilon);
-    EXPECT_LT(std::abs(computedStats.min - 1.5), epsilon);
+TEST(StatisticsTest, ComputesAverageMinMax) {
+    std::vector<double> numbers = {1.5, 8.9, 3.2, 4.5};
+    Stats s = Statistics::ComputeStatistics(numbers);
+
+    double expectedAvg = (1.5 + 8.9 + 3.2 + 4.5) / 4.0;
+    EXPECT_DOUBLE_EQ(s.average, expectedAvg);
+    EXPECT_DOUBLE_EQ(s.min, 1.5);
+    EXPECT_DOUBLE_EQ(s.max, 8.9);
 }
 
-TEST(Statistics, AverageNaNForEmpty) {
-    auto computedStats = Statistics::ComputeStatistics({});
-    // All fields of computedStats (average, max, min) must be
-    // NAN (not-a-number), as defined in math.h
-    
-    // Specify the EXPECT statement here.
-    // Use http://www.cplusplus.com/reference/cmath/isnan/
+TEST(StatisticsTest, ReturnsNaNForEmptyVector) {
+    std::vector<double> numbers; // empty
+    Stats s = Statistics::ComputeStatistics(numbers);
+
+    EXPECT_TRUE(std::isnan(s.average));
+    EXPECT_TRUE(std::isnan(s.min));
+    EXPECT_TRUE(std::isnan(s.max));
 }
